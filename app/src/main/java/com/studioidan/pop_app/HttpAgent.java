5155924c0f2ee954ee12e746d5dd2c6c518fe1cd
idan ben shimon <studioidan@gmail.com>
2015-11-08 23:33:10 +0200

    minor change

--- app/src/main/java/com/studioidan/pop_app/HttpAgent.java
+++ app/src/main/java/com/studioidan/pop_app/HttpAgent.java
@@ -178,6 +178,7 @@ public class HttpAgent extends AsyncTask<String, String, String> {
                 }
             } else
                 mCallback.onRequestEnd(mMethodName, res);
+            //minor change
 
         }
     }
